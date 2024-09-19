e3052cd819c0450e77d32cca4c2eebd02ed3379a
Annyaii <annaitkace200712@gmail.com>
2024-09-19 14:24:01 +0300

    Update opp1.py

--- opp1.py
+++ opp1.py
@@ -1,13 +1,17 @@
 class person:
     def set(self,name,age):
-        self.name = name
-        self.age= age
+        self.name=name
+        self.age=age
     def output(self):
         print(f'Persona {self.name}, kuram ir {self.age} gadi')
+    def labdien(self):
+        print(f'Labdien, {self.name}!')
 
 p = person()
 p.set("Peter",20)
 p.output()
+p.labdien()
 a= person()
 a.set("Anna",16)
 a.output()
+a.labdien()
