352682a80f29a138b104a6ce2c6439bafb555752
Annyaii <annaitkace200712@gmail.com>
2024-10-17 14:30:32 +0300

    Update muzikauzd.py

--- muzikauzd.py
+++ muzikauzd.py
@@ -8,13 +8,16 @@ class Instrument:
     
       if cost > 100:
         summa=cost*0,2+cost
-            
-       
+    def inform_count():
+        print(f'Instancescreated: {Instrument.count}')
+   def introduce(self):
+        print(f'{self.name}{self.tip}{self.cost}')   
 
 
 inst1=Instrument('klavieres','taustiņinstruments',150)
 print(f'{inst1.name} {inst1.tip} {inst1.cost}')
 print(f'cena ir {summa} ')
+Instrument.inform_count()
 
 
    
